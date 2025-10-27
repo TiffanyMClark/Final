550167fcd008ad3e6c0c5c5023e5658c9d85068b
tiffany clark <tiffanymarie9811@gmail.com>
2025-10-27 15:02:48 -0400

    added process.env.database to server js. to hopefully only run if DB is defined

--- src/server.js
+++ src/server.js
@@ -4,11 +4,18 @@ const app = require("./app");
 const knex = require("./db/connection");
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
+// knex has a render issue? build is fine but it wont deploy..
+// nneed to run migrations if the database is defined
 
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
-  })
-  .catch(console.error);
+if (process.env.DATABASE_URL) {
+  knex.migrate
+    .latest()
+    .then((migrations) => {
+      console.log("migrations", migrations);
+      app.listen(PORT, listener);
+    })
+
+    .catch(console.error);
+} else {
+  app.listen(PORT, listener);
+}
